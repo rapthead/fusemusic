80d64ba5fedb9678a3fac185060a0a583d53cb43
rapthead <rapthead@gmail.com>
2015-06-25 22:07:31 +0500

    urgency v2 - orm rose::db::object

--- urgency.pl
+++ urgency.pl
@@ -19,7 +19,8 @@ use FindBin;
 use lib "$FindBin::RealBin/lib";
 
 use MusicDB;
-use CueParse;
+#use CueParse;
+use CueParse2;
 
 my $music_lib_dir = '/home/music/lossless/';
 my $covers_dir = $ENV{'HOME'}.'/covers/';
@@ -64,10 +65,10 @@ sub update_track($$) {
     { die "изменилась продолжительность файла [", $track_info->{uri}, "], требуется ручное исправление ошибки\n" }
 
     my $is_track_changed = 0;
-    my @fields = ('title', 'disc', 'track_artist');
+    my @fields = ('title', 'disc', 'track_artist', 'rg_peak', 'rg_gain');
     foreach my $field (@fields) {
         if ($track_info->{$field} ne $track->$field) {
-            warnchanges($field, $track_info->{$field}, $track->$field);
+            warnchanges($field, $track->$field, $track_info->{$field});
             $track->$field($track_info->{$field});
             $is_track_changed = 1;
         }
@@ -90,54 +91,76 @@ sub warnchanges($$$) {
 sub main {
     my @cueFiles;
     sub wanted { push(@cueFiles,$File::Find::name) if (/\.cue$/ && -r); }
-    find(\&wanted, $music_lib_dir);
-    #.'/cd/what.cd/Razika/');
+    find(\&wanted, $music_lib_dir);#.'/cd/what.cd/311/');
     @cueFiles = sort @cueFiles;
 
     foreach (@cueFiles) {
         my $cueFile = $_;
         my $cueDir = dirname($cueFile);
-        my $cueHash = parse($cueFile);
+        my $cueHash = parseCue($cueFile);
 
         #artUrgency($cueDir, $cueHash->{'artist'}, $cueHash->{'album'});
-        $cueHash->{'date'} =~ m/(\d{4})(?:-(\d{4}))?/;
+        $cueHash->{'album'}->{'REM'}->{'DATE'} =~ m/(\d{4})(?:-(\d{4}))?/;
         my $cue_info = {
             tracks => [],
             album => {
-                genre       => $cueHash->{genre},
-                title       => $cueHash->{'album'},
+                genre       => $cueHash->{'album'}->{'REM'}->{'GENRE'},
+                title       => $cueHash->{'album'}->{'TITLE'},
                 date        => DateTime->new(year => $1),
                 release_date=> DateTime->new(year => $2 || $1),
                 is_changed  => 0,
                 db_object   => undef
             },
             artist => {
-                name => $cueHash->{'artist'},
+                name => $cueHash->{'album'}->{'PERFORMER'},
                 is_changed  => 0,
                 db_object   => undef
             }
         };
+        $cueHash->{'album'}->{'REM'}->{'REPLAYGAIN_ALBUM_GAIN'} =~ s/\s*db$//;
+        $cue_info->{'album'}->{'rg_peak'} = $cueHash->{'album'}->{'REM'}->{'REPLAYGAIN_ALBUM_PEAK'}+0
+            if ($cueHash->{'album'}->{'REM'}->{'REPLAYGAIN_ALBUM_PEAK'});
+        $cue_info->{'album'}->{'rg_gain'} = $cueHash->{'album'}->{'REM'}->{'REPLAYGAIN_ALBUM_GAIN'}+0
+            if ($cueHash->{'album'}->{'REM'}->{'REPLAYGAIN_ALBUM_GAIN'});
+
         warn "processing: ", $cue_info->{artist}->{name}, ' - ',$cue_info->{album}->{title},"\n";
 
         foreach my $track (@{$cueHash->{'tracks'}}) {
-            my $trackFile = File::Spec->catfile($cueDir,$track->{'file'});
-            if (! -e $trackFile) { warn "Файл [", $cueDir.$track->{'file'}, "] не найден!\n"; }
-            elsif (! -f $trackFile) { warn "[", $cueDir.$track->{'file'}, "] не является файлом!\n"; }
-            else {
-                my $flacHeader = Audio::FLAC::Header->new($trackFile);
-                my $trackUri = File::Spec->abs2rel($trackFile,$music_lib_dir);
-                push @{$cue_info->{tracks}}, {
-                    track_artist=> ($track->{'performer'} eq $cueHash->{'artist'}) ?
-                        '':($track->{'performer'} || $cueHash->{'artist'}),
-                    title       => $track->{'title'},
-                    track_num   => $track->{'track_no'},
-                    disc        => $cueHash->{discnumber} || 1,
-                    length      => ceil($flacHeader->{'trackTotalLengthSeconds'}),
-                    uri         => $trackUri,
-                    processed   => 0,
-                    is_changed  => 0,
-                    db_object   => undef
+            my $trackFile = '';
+            if (defined $track->{'INDEX'}->[1] && $track->{'track'}->{'datatype'} == 'AUDIO') {
+                $trackFile = $cueHash->{'files'}->[$track->{'INDEX'}->[1]->{'fileindex'}]->{'filename'};
+
+                my $trackFilePath = File::Spec->catfile($cueDir,$trackFile);
+                if (! -e $trackFilePath) { warn "Файл [", $trackFilePath, "] не найден!\n"; }
+                elsif (! -f $trackFilePath) { warn "[", $trackFilePath, "] не является файлом!\n"; }
+                else {
+                    my $flacHeader = Audio::FLAC::Header->new($trackFilePath);
+                    my $trackUri = File::Spec->abs2rel($trackFilePath,$music_lib_dir);
+                    my $trackInfo = {
+                        track_artist=> ($track->{'PERFORMER'} eq $cueHash->{'album'}->{'PERFORMER'}) ?
+                            '':($track->{'PERFORMER'}),
+                        title       => $track->{'TITLE'},
+                        track_num   => $track->{'TRACK'}->{'number'},
+                        disc        => $cueHash->{'album'}->{'REM'}->{'DISCNUMBER'} || 1,
+                        length      => ceil($flacHeader->{'trackTotalLengthSeconds'}),
+                        uri         => $trackUri,
+                        processed   => 0,
+                        is_changed  => 0,
+                        db_object   => undef
+                    };
+                    $track->{'REM'}->{'REPLAYGAIN_TRACK_GAIN'} =~ s/\s*db$//;
+                    $trackInfo->{'rg_peak'} = $track->{'REM'}->{'REPLAYGAIN_TRACK_PEAK'}+0
+                        if ($track->{'REM'}->{'REPLAYGAIN_TRACK_PEAK'});
+                    $trackInfo->{'rg_gain'} = $track->{'REM'}->{'REPLAYGAIN_TRACK_GAIN'}+0
+                        if ($track->{'REM'}->{'REPLAYGAIN_TRACK_GAIN'});
+                    push @{$cue_info->{tracks}}, $trackInfo;
                 }
+            }
+            elsif ($track->{'TRACK'}->{'datatype'} ne 'AUDIO') {
+                warn "Трек ".$track->{'TRACK'}->{'number'}." не является аудио-файлом\n";
+            }
+            else {
+                warn "У трека ".$track->{'TRACK'}->{'number'}." нет файла\n";
             }
         }
 
@@ -151,7 +174,11 @@ sub main {
         foreach my $db_track (@$db_track_search_by_uri) {
             my @cue_tracks = grep { $_->{uri} eq $db_track->uri } @{$cue_info->{tracks}};
             # трек не найден
-            if (@cue_tracks!=1) { die "Что-то пошло не так, треки с одинаковым uri\n"; }
+            if (@cue_tracks!=1) {
+				print Dumper $cueHash;
+				#print Dumper $cue_info->{tracks};
+				die "Что-то пошло не так, треки с одинаковым uri\n"; 
+			}
 
             my $cur_track = $cue_tracks[0];
             if (update_track($db_track, $cur_track)) {
@@ -193,7 +220,7 @@ sub main {
             # не найдено соответствие в базе
             else {
                 $cur_track->{db_object} = Track->new();
-                foreach (('track_artist', 'title', 'track_num', 'disc', 'length', 'uri')) {
+                foreach (('track_artist', 'title', 'track_num', 'disc', 'length', 'uri', 'rg_peak', 'rg_gain')) {
                     warnchanges($_,'',$cur_track->{$_});
                     $cur_track->{db_object}->$_($cur_track->{$_});
                 }
@@ -215,7 +242,7 @@ sub main {
                 if ( $cue_info->{artist}->{db_object} ) {
                     $cue_info->{album}->{db_object}->artist($cue_info->{artist}->{db_object});
                 }
-                else { $cue_info->{album}->{db_object}->artist(Artist->new()); }
+                else { $cue_info->{album}->{db_object}->artist(Artist->new('name' => $cue_info->{artist}->{name})); }
             }
 
             $cue_info->{artist}->{db_object} = $cur_track->{db_object}->album->artist
@@ -223,7 +250,7 @@ sub main {
         }
 
         my $album = $cue_info->{album}->{db_object};
-        my @fields = ('title', 'date', 'release_date', 'genre');
+        my @fields = ('title', 'date', 'release_date', 'genre', 'rg_peak', 'rg_gain');
         foreach my $field (@fields) {
             if ($cue_info->{album}->{$field} ne $album->$field) {
                 warnchanges("album.".$field,$album->$field?$album->$field:'',$cue_info->{album}->{$field});
@@ -236,12 +263,15 @@ sub main {
         if ($artist->name ne $cue_info->{artist}->{name}) {
             warnchanges("artist.name",$artist->name,$cue_info->{artist}->{name});
             $artist->name($cue_info->{artist}->{name});
+            $artist->artist_id(undef);
             $cue_info->{artist}->{is_changed} = 1;
             $cue_info->{album}->{is_changed} = 1;
         }
 
         if ($cue_info->{artist}->{is_changed}) { 
+            #$Rose::DB::Object::Debug = 1;
             $artist->save( insert => 1 );
+            #$Rose::DB::Object::Debug = 0;
 
             $album->artist_id($artist->artist_id());
             $cue_info->{album}->{is_changed} = 1;
